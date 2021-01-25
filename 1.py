25813c84fce43d54b7989df3f294c9167ee4b661
김태수 <kimts@brandi.co.kr>
2021-01-26 07:51:29 +0900

    210126 LeetCode#1

---
+++ 1.py
@@ -0,0 +1,22 @@
+import itertools
+
+class Solution:
+    def twoSum(self, nums, target):
+        combi = itertools.combinations(nums, 2)
+        result = ''
+        for i in combi:
+            if sum(i) == target:
+                result = i
+                break
+        
+        result_1 = ''
+        result_2 = ''
+        
+        for i, v in enumerate(nums):
+            if v == result[0] and result_1 == '':
+                result_1 = i
+            if v == result[1] and result_1 != i and result_2 == '':
+                result_2 = i
+                
+        return [result_1, result_2]
+        
